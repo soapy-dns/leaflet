f1a2e1d1e5bc8dcc0c17a3ee21648547c6b1fe1a
soapy-dns <neil.soutar@iag.com.au>
2018-02-08 18:36:29 +1100

    rfs major incidents

--- client/app/reducers/tracks.js
+++ client/app/reducers/tracks.js
@@ -4,11 +4,8 @@ import { getLine } from '../utils/index'
 export default function (trackState = [], action) {
     switch (action.type) {
         case SAVE_TRACK:
-            console.log('trackState', trackState)
             trackState.map(it => it.selected = false)
-            console.log('SAVE_TRACK track', action.track)
             const obj = { track: action.track, selected: true }
-            console.log(`obj ${obj}`)
             trackState.push(obj)
             return trackState
 
