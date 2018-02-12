b13d27e5438173e2fcc031bb138952551880b3e2
soapy-dns <neil.soutar@iag.com.au>
2018-02-12 20:22:19 +1100

    fiddle with webpack and server.js file for working deployed version

--- server.js
+++ server.js
@@ -1,37 +1,39 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const environment = process.env.NODE_ENV || 'development';
-const port = process.env.PORT || 9000;
-const app = express();
+const environment = process.env.NODE_ENV || 'development'
+const port = process.env.PORT || 9000
+const app = express()
 
 function ensureSecure(req, res, next) {
     if (req.headers['x-forwarded-proto'] === 'https') {
         // OK, continue
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        return next();
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        return next()
     }
     if (environment === 'production') {
-        return res.redirect(301, 'https://' + req.headers.host + req.url); // eslint-disable-line
+        return res.redirect(301, 'https://' + req.headers.host + req.url) // eslint-disable-line
     }
-    return next();
+    return next()
 }
 
 // force https for production
-app.use(ensureSecure);
+app.use(ensureSecure)
 
 // serve static assets normally
-app.use(express.static(path.join(__dirname, '/')));
-app.all('/', (req, res, next) => {
-    console.log('path-', req.path)
-    next()
-})
+app.use(express.static(path.join(__dirname, '/')))
+console.log('__dirname', __dirname)
+app.use('/bundle.js', express.static(path.join(__dirname, '/dist/bundle.js')))
+
 
 // Handles all routes so you do not get a not found error
 app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, '../dist/index.html'));
-});
+    console.log('__dirname', __dirname)
+    response.sendFile(path.join(__dirname, '/dist/index.html'))
+    // response.sendFile(path.join(__dirname, 'app/index.html'))
 
-app.listen(port);
-console.log(`server started on port ${port}`);
+})
+
+app.listen(port)
+console.log(`server started on port ${port}`)
