5cb70ec53c30415611d972dffca9e5f4e9077410
soapy-dns <neil.soutar@iag.com.au>
2018-02-17 12:37:59 +1100

    Procfile to start app from dist folder, change .gitignore to allow dist folder to be pushed to git.  change server.js for running off /dist

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const express = require('express')
-const path = require('path')
+'use strict'
 
-const environment = process.env.NODE_ENV || 'development'
+const express = require("express")
+const path = require("path")
+
+const environment = process.env.NODE_ENV || "development"
 const port = process.env.PORT || 9000
 const app = express()
 
 function ensureSecure(req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'https') {
+    if (req.headers["x-forwarded-proto"] === "https") {
         // OK, continue
-        res.header('Access-Control-Allow-Origin', '*')
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.header("Access-Control-Allow-Origin", "*")
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        )
         return next()
     }
-    if (environment === 'production') {
-        return res.redirect(301, 'https://' + req.headers.host + req.url) // eslint-disable-line
+    if (environment === "production") {
+        return res.redirect(301, "https://" + req.headers.host + req.url) // eslint-disable-line
     }
     return next()
 }
@@ -22,17 +27,15 @@ function ensureSecure(req, res, next) {
 app.use(ensureSecure)
 
 // serve static assets normally - I guess static assets are files
-app.use(express.static(path.join(__dirname, '/dist/')))
-console.log('__dirname', __dirname)
+app.use(express.static(path.join(__dirname, "/dist/")))
+console.log("__dirname", __dirname)
 // app.use('/bundle.js', express.static(path.join(__dirname, '/dist/bundle.js')))
 
-
 // Handles all routes so you do not get a not found error
-app.get('*', (request, response) => {
-    console.log('__dirname', __dirname)
-    response.sendFile(path.join(__dirname, '/dist/index.html'))
+app.get("*", (request, response) => {
+    console.log("__dirname", __dirname)
+    response.sendFile(path.join(__dirname, "/dist/index.html"))
     // response.sendFile(path.join(__dirname, 'app/index.html'))
-
 })
 
 app.listen(port)
