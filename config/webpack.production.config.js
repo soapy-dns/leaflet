d09e39c6e1650a44d8d6c35e5edc3a4647f59030
soapy-dns <neil.soutar@iag.com.au>
2018-02-17 13:17:30 +1100

    removed undefined and unused node module

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const Config = require('webpack-config').default;
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = new Config().extend('config/webpack.base.config.js').merge({
     module: {
