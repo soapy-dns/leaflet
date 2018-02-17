24d3651291fce382dafd662a432c3b90f36932d4
soapy-dns <neil.soutar@iag.com.au>
2018-02-17 13:43:20 +1100

    webpack plugin changes

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -4,7 +4,21 @@ const Config = require('webpack-config').default;
 module.exports = new Config().extend('config/webpack.base.config.js').merge({
     module: {
 
-    },
+        rules: [
+            // allows importing of css into the file rather than having script tags in index.html
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+        // rules: [{
+        //     test: /\.css$/,
+        //     loader: ['style-loader', 'css-loader?importLoaders=1&sourceMap', 'postcss-loader']
+        // }]
+    }
     // plugins: [
     //     new webpack.optimize.UglifyJsPlugin({
     //         compress: {
